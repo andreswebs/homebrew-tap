60381039774da03a14e31c13c447bd64ffee68f4
Andre Silva <andreswebs@pm.me>
2025-10-13 17:27:11 -0300

    mcbox 0.1.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mcbox.rb
+++ Formula/mcbox.rb
@@ -1,8 +1,8 @@
 class Mcbox < Formula
   desc "A pluggable MCP server in Bash and jq"
   homepage "https://andreswebs.github.io/mcbox"
-  url "https://github.com/andreswebs/mcbox/releases/download/0.1.4/mcbox-0.1.4.tar.gz"
-  sha256 "7f6cb70cf51db377f780c95dc98983e45204c057dd57e313dd9bb4e99084419c"
+  url "https://github.com/andreswebs/mcbox/releases/download/0.1.5/mcbox-0.1.5.tar.gz"
+  sha256 "fc9fa65f51ad82ff3d14cf467d1f2e575ba3857025317ce90c2c961427eea828"
   license "GPL-3.0-or-later"
 
   depends_on "bash"
