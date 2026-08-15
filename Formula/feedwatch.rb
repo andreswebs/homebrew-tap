a4d642a808fefd97bf223f71324644938689dbe2
Andre Silva <andreswebs@pm.me>
2026-08-15 00:36:59 -0300

    upd feedwatch
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- Formula/feedwatch.rb
+++ Formula/feedwatch.rb
@@ -1,24 +1,24 @@
 class Feedwatch < Formula
   desc "Agent-first command-line tool for watching RSS and Atom feeds"
   homepage "https://github.com/andreswebs/feedwatch"
-  version "0.0.4"
+  version "0.0.5"
   license "GPL-3.0-or-later"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/andreswebs/feedwatch/releases/download/v#{version}/feedwatch-darwin-arm64-v#{version}.tar.gz"
-      sha256 "e4e5aca28acd78ef6eb3bb838f61d0a910e862b50f99c3d4f0ca37d7d320adc9"
+      sha256 "a293dbb738cceeae1f39bffa8f56ef91cfc19eda2a1ca8aa608f4d2ae36ec2f3"
     else
       url "https://github.com/andreswebs/feedwatch/releases/download/v#{version}/feedwatch-darwin-amd64-v#{version}.tar.gz"
-      sha256 "4674b53c49c35ff89b5d46a9e95c2b461ca46c7bc8a609c46521f54d161e854b"
+      sha256 "e4d368fa7663d09476ba79a37161d86fdee991fe6fc85a5fc242a256b0a333d6"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/andreswebs/feedwatch/releases/download/v#{version}/feedwatch-linux-arm64-v#{version}.tar.gz"
-      sha256 "cd0f74bbd43b479ac5a0827d1d3bdbc8fbf6bc09adc71ddd9f47cde5c5b31e16"
+      sha256 "4807247d050bc8feeabdb7369d9117ccb5c06835e32f1fac35c6ec2b23ca2062"
     else
       url "https://github.com/andreswebs/feedwatch/releases/download/v#{version}/feedwatch-linux-amd64-v#{version}.tar.gz"
-      sha256 "97fabe35b99024fd0357b50fb1c21d58cf3928ed543c0bf94d82a543051dabc0"
+      sha256 "9d1fecc18a1b45d5e80e2d5a0ef6df7655c8243bde01fd6d7060c8dab447b4c9"
     end
   end
 
